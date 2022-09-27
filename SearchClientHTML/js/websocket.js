76d56cd87b3ee2163c6e10516d7d27d4f756424e
Sely <mailmesalah@gmail.com>
2022-09-27 16:03:36 +0100

    First Commit

---
+++ SearchClientHTML/js/websocket.js
@@ -0,0 +1,41 @@
+
+// the backbone of the application: the HTML5 web socket!
+var socket;
+
+/**
+ * register callbacks on the socket
+ */
+function onOpen(e){ 
+  var data = document.getElementById("textSearch").value;
+  socket.send(data);    
+}
+
+function onClose(e){
+}
+
+function onMessage(e){    	
+   document.getElementById("divFooter").innerHTML=e.data;   
+   socket.close();
+  
+}
+function onError(e) { 	
+}
+
+//Methods
+
+function connectToServer(){	
+	var hostIPAdd="127.0.0.1";
+	var portNo="8080";
+	//Connecting with server
+	socket = new WebSocket("ws://"+hostIPAdd+":"+portNo);	
+	socket.onopen = function(evt) { onOpen(evt) }; 
+	socket.onclose = function(evt) { onClose(evt) }; 
+	socket.onmessage = function(evt) { onMessage(evt) }; 
+	socket.onerror = function(evt) { onError(evt) };		
+}
+
+function onClicked() { 
+   connectToServer(); 
+   event.preventDefault();
+   return false;
+}
---
+++ SearchServerCSharp/Search/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Alchemy;
+using Alchemy.Classes;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            WebSocketServer aServer = new WebSocketServer(8080, IPAddress.Any)
+            {
+                OnReceive = OnReceive,
+                OnConnect = OnConnect,
+                OnConnected = OnConnected,
+                OnDisconnect = OnDisconnect,
+                //Time out in 0 hours 5 minute 0 seconds
+                TimeOut = new TimeSpan(0, 10, 0)
+            };
+
+            aServer.Start();
+            Console.WriteLine("Server Starts");
+
+        }
+
+        private static void OnDisconnect(UserContext context)
+        {
+            Console.WriteLine("Client Disconnects");
+        }
+
+        private static void OnConnect(UserContext context)
+        {
+            Console.WriteLine("Server Connects");
+        }
+
+
+        private static void OnReceive(UserContext context)
+        {
+
+            Console.WriteLine("Server Receive" + context.DataFrame);
+            context.Send("haaaifffsdhhshhsasaaaaaaaaaaaaaaaaaaaaaaaaaaaaassssssssssssssssffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssnnnnnnnnnnnnnnnnnnnnx");
+        }
+
+        static void OnConnected(UserContext aContext)
+        {
+            Console.WriteLine("Client Connection From : " + aContext.ClientAddress.ToString());
+        }
+
+    }
+}
